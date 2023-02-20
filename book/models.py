d889f9cd7378b97c309272a8b77b39a54e105ff8
Forix <forixmehdi@gmail.com>
2023-02-20 19:22:25 +0000

    Added missing comments

--- book/models.py
+++ book/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-# Create your models here.
-
+#This is the model that will structure how book information will be saved in the database.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_date = models.DateTimeField('date published')
     num_pages = models.IntegerField()
     def __str__(self):
-        return self.title
+        return "{ title=("+self.title+"), author=("+self.author+", num_pages=("+self.num_pages+"), publication_date=("+self.publication_date+")}"
--- book/urls.py
+++ book/urls.py
@@ -1,12 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 app_name = 'book'
 urlpatterns = [
-    path('', views.LibraryBook.as_view(), name='index'),
-    path('<int:num>/', views.book, name='book'),
-    path('create/', views.createBook, name='create'),
-    path('edit/<int:num>', views.editBook, name='edit'),
-    path('delete/<int:num>', views.confirmDelete, name='delete'),
+    path('', views.LibraryBook.as_view(), name='index'), #The view for the main page where all books will be displayed
+    path('<int:num>/', views.book, name='book'), #The view where a specific book informations will be displayed
+    path('create/', views.createBook, name='create'), #The view for creating a book
+    path('edit/<int:num>', views.editBook, name='edit'), #The view that will edit an already existing book
+    path('delete/<int:num>', views.confirmDelete, name='delete'), #The 'ghost' view that will delete a book
 ]
